6094b316cf04e34b6e50233949f85045ba74e6dd
Griphcode <148160412+GriphcodeDev@users.noreply.github.com>
2025-02-20 01:28:20 +0100

    New changes
    Layout and functionality

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -2,24 +2,91 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        private double storage = 0;
+        private double operand = 0;
+        private string operation = "";
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void NumberBtn(object sender, EventArgs e)
         {
-            count++;
+            Button button = (Button)sender;
+            operand = (operand * 10) + Convert.ToDouble(button.Text);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            Calculation.Text += button.Text;
         }
-    }
 
-}
+        private void OperatorBtn(object sender, EventArgs e)
+        {
+            if (operation != "")
+            {
+                Calculate();
+            }
+            else
+            {
+                storage = operand;
+            }
+
+            operand = 0;
+
+            Button button = (Button)sender;
+            operation = button.Text;
+
+            Calculation.Text += $"{operation}";
+        }
+
+        private void EqualBtn(object sender, EventArgs e)
+        {
+            Calculate();
+
+            Calculation.Text = storage.ToString();
+
+            operation = "";
+            operand = 0;
+        }
+
+        private void Calculate()
+        {
+            switch (operation)
+            {
+                case "+":
+                    storage += operand;
+                    break;
+                case "-":
+                    storage -= operand;
+                    break;
+                case "*":
+                    storage *= operand;
+                    break;
+                case "/":
+                    if (operand == 0)
+                    {
+                        Clear();
+                        return;
+                    }
+                    storage /= operand;
+                    break;
+            }
+            operand = 0;
+        }
+
+        private void ClearBtn(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            storage = 0;
+            operand = 0;
+            operation = "";
+
+            Calculation.Text = "";
+        }
+
+
+    }
+} 
